63a8e40a2910d247ec29f7e95b33359335d8f820
Luis C. Gutierrez <luis.c.gutierrez.h@gmail.com>
2014-01-06 14:42:56 -0700

    Initialize xcode project

---
+++ main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Realpaver-0_4-Mac
+//
+//  Created by Luis on 1/6/14.
+//  Copyright (c) 2014 Luis. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
